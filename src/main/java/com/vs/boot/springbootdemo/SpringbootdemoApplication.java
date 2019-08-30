5658272aee69c86eb4c465b5bf65dd9444c629ca
gyansri <48654215+gyansri@users.noreply.github.com>
2019-08-30 22:04:02 +0530

    Update SpringbootdemoApplication.java

--- src/main/java/com/vs/boot/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/vs/boot/springbootdemo/SpringbootdemoApplication.java
@@ -11,6 +11,7 @@ public class SpringbootdemoApplication {
 		System.out.println("2 ");
 		System.out.println("3 ");
 		System.out.println("4 ");
+		System.out.println("5 ");
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
